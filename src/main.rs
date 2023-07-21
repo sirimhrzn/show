bea65955450a6ecfc887d38fbf0a03f3df0f9f0c
Shirish Maharjan <shirishmaharjan@gmail.com>
2023-07-21 22:44:37 +0545

    query works,code refactor,fixed error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env::{self},
+    env,
     error::Error,
     fs::{self, File},
     process,
@@ -17,7 +17,6 @@ struct Grep<'a> {
     query: Vec<String>,
     args: &'a Vec<String>,
     file_path: String,
-    arg_index: usize,
     flags: Vec<String>, // supported_types: i32,
 }
 fn main() -> Result<(), Box<dyn Error>> {
@@ -33,44 +32,43 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 impl<'a> Grep<'a> {
-    fn new(options: Vec<GrepOptions>, args: &'a Vec<String>) -> Grep {
+    fn new(options: Vec<GrepOptions>, args: &'a Vec<String>) -> Self {
         let grep = Grep {
             options,
             args,
             file_path: "".to_string(),
             query: vec!["".to_string()],
-            arg_index: 0,
             flags: vec!["-F".to_string(), "-U".to_string(), "-L".to_string()],
         };
         return grep;
     }
-    fn file_checker(path: &String) -> Result<&'static str, &'static str> {
-        let file = File::open(path);
+    fn file_checker(&self) -> Result<(), &'static str> {
+        let file = File::open(&self.file_path);
         match file {
-            Ok(e) => Ok("file path valid"),
-            Err(e) => Err("Siri error bhayo"),
+            Ok(_) => Ok(()),
+            Err(_) => Err("Invalid file path or File not found"),
         }
     }
-    fn search(query: &String) -> Result<(), &'static str> {
-        let file = fs::read_to_string("src/sir.txt").map_err(|e| "soro")?;
-        dbg!(query);
+    fn search(&self) -> Result<(), &'static str> {
+        self.file_checker()?;
+        let query = &self.query_parser();
+        let file = fs::read_to_string(&self.file_path);
         let mut matches = Vec::new();
-        for a in file.lines() {
-            if a.contains("second") {
-                matches.push(a);
+        match file {
+            Ok(content) => {
+                for line in content.lines() {
+                    if line.contains(query) {
+                        matches.push(line);
+                    }
+                }
+                println!("{:?}", matches);
+                Ok(())
             }
+            Err(_) => Err("Unable to read data"),
         }
-        println!("{:?}", matches);
-        Ok(())
     }
-    fn query_parser(query: &Vec<String>) -> Result<(), &'static str> {
-        let mut queries = String::new();
-        for q in query {
-            queries = queries + " " + &q;
-        }
-        let search = Self::search(&queries)?;
-        // println!(" these ate {queries}");
-        Ok(())
+    fn query_parser(&self) -> String {
+        self.query.join(" ")
     }
     fn index_return(val: &str, ind: &'a Vec<String>) -> Result<usize, &'static str> {
         let index = ind.iter().position(|x| x == val);
@@ -79,50 +77,32 @@ impl<'a> Grep<'a> {
             None => return Err(""),
         }
     }
-    fn args_parser(grep: &mut Grep) -> Result<Vec<String>, &'static str> {
-        let mut query: Vec<&String> = Vec::new();
-        let mut first = 0;
-        if !grep.args.contains(&"-F".to_string()) {
+    fn args_parser(&mut self) -> Result<Vec<String>, &'static str> {
+        if !self.args.contains(&"-F".to_string()) {
             return Err("File path not specified");
         }
-        // let que ;
-        for a in grep.args {
-            match a.as_str() {
+        for flag in self.args {
+            match flag.as_str() {
                 "-F" => {
-                    let option_index = Grep::index_return("-F", grep.args)?;
-                    if grep.args.len() - 1 == option_index {
+                    let option_index = Self::index_return("-F", self.args)?;
+                    if self.args.len() - 1 == option_index {
                         return Err("No file path specified");
                     }
-                    let file = grep.args[option_index + 1].to_string();
-                    grep.query = grep.args[1..option_index].to_vec();
-                    grep.file_path = file;
+                    let file = self.args[option_index + 1].to_string();
+                    self.query = self.args[1..option_index].to_vec();
+                    self.file_path = file;
                 }
                 "-L" => {
-                    grep.options.push(GrepOptions::LowerCase);
+                    self.options.push(GrepOptions::LowerCase);
                 }
                 "-U" => {
-                    grep.options.push(GrepOptions::UpperCase);
+                    self.options.push(GrepOptions::UpperCase);
                 }
-                _ => {
-                    let option_index = Grep::index_return("-F", grep.args)?;
-                    // katy samma arguments line
-                    // let mut ind = 0;
-                    // if first != 0 {
-                    // if ind < option_index  {
-                    //       query.push(a);
-                    // query.pop();
-                    //     ind += 1;
-                    //}
-                    // }
-                }
+                _ => {}
             }
-            // first = 1;
         }
 
-        let _file_path = Grep::file_checker(&grep.file_path)?;
-        // query.pop();
-        let _quer_p = Grep::query_parser(&grep.query);
-
+        let _search = Self::search(&self)?;
         Ok(vec!["gee".to_string()])
     }
 }
