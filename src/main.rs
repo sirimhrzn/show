63390f518203fcc3bb1b7e3f0a277eaba32fbdf2
Shirish Maharjan <shirishmaharjan@gmail.com>
2023-07-22 11:32:19 +0545

    created enum for errors

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,42 @@ enum GrepOptions {
 }
 
 #[derive(Debug)]
+enum GrepError {
+    FileNotFound,
+    QueryNotFound,
+    ReadFailed,
+    NoQuerySpecified,
+    NoFilePathSpecified,
+    NoFlagSpecified,
+    CustomError(&'static str),
+}
+
+#[derive(Debug)]
 struct Grep<'a> {
     options: Vec<GrepOptions>,
     query: Vec<String>,
     args: &'a Vec<String>,
     file_path: String,
-    flags: Vec<String>, // supported_types: i32,
+    flags: Vec<String>,
 }
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args = env::args();
     let values: Vec<String> = args.collect();
     let mut grepper = Grep::new(vec![GrepOptions::Default], &values);
 
     if let Err(e) = Grep::args_parser(&mut grepper) {
-        println!("{e}");
+        match e {
+            GrepError::FileNotFound => println!("File not found"),
+            GrepError::QueryNotFound => println!("No match found for the given query"),
+            GrepError::ReadFailed => println!("Failed reading document.Invalid file type"),
+            GrepError::NoQuerySpecified => println!("No query given. Please run --help for info"),
+            GrepError::NoFilePathSpecified => println!("No file path given "),
+            GrepError::NoFlagSpecified => {
+                println!("No flags specified. Please run --help for info")
+            }
+            GrepError::CustomError(error) => println!("{error}"),
+        }
         process::exit(1);
     }
     Ok(())
@@ -41,21 +63,21 @@ impl<'a> Grep<'a> {
             flags: vec!["-F".to_string(), "-U".to_string(), "-L".to_string()],
         }
     }
-    fn file_checker(&self) -> Result<(), &'static str> {
+    fn file_checker(&self) -> Result<(), GrepError> {
         let file = File::open(&self.file_path);
         match file {
             Ok(_) => Ok(()),
-            Err(_) => Err("File not found"),
+            Err(_) => Err(GrepError::FileNotFound),
         }
     }
-    fn search(&mut self) -> Result<(), &'static str> {
+    fn search(&mut self) -> Result<(), GrepError> {
         if self.query.is_empty()
             && self
                 .options
                 .iter()
                 .any(|a| matches!(a, GrepOptions::Default))
         {
-            return Err("Query is empty");
+            return Err(GrepError::NoQuerySpecified);
         }
         self.file_checker()?;
         let query = self.query_parser();
@@ -69,34 +91,35 @@ impl<'a> Grep<'a> {
                     }
                 }
                 if matches.is_empty() {
-                    return Err("No matches found ");
+                    return Err(GrepError::QueryNotFound);
                 }
+                println!(" {} matches found", matches.len());
                 println!("{:?}", matches);
                 Ok(())
             }
-            Err(_) => Err("Unable to read data"),
+            Err(_) => Err(GrepError::ReadFailed),
         }
     }
     fn query_parser(&self) -> String {
         self.query.join(" ")
     }
-    fn index_return(&self) -> Result<usize, &'static str> {
+    fn index_return(&self) -> Result<usize, GrepError> {
         let index = self.args.iter().position(|x| x == &self.flags[0]);
         match index {
             Some(i) => Ok(i),
-            None => return Err("Error indexing flag. No flag specified."),
+            None => return Err(GrepError::CustomError("Invalid index")),
         }
     }
-    fn args_parser(&mut self) -> Result<Vec<String>, &'static str> {
+    fn args_parser(&mut self) -> Result<Vec<String>, GrepError> {
         if !self.args.iter().any(|arg| self.flags.contains(&arg)) {
-            return Err("No flags specified");
+            return Err(GrepError::NoFlagSpecified);
         }
         for flag in self.args {
             match flag.as_str() {
                 "-F" => {
                     let option_index = self.index_return()?;
                     if self.args.len() - 1 == option_index {
-                        return Err("No file path specified");
+                        return Err(GrepError::NoFilePathSpecified);
                     }
                     let file = self.args[option_index + 1].to_string();
                     self.query = self.args[1..option_index].to_vec();
@@ -116,4 +139,3 @@ impl<'a> Grep<'a> {
         Ok(vec!["gee".to_string()])
     }
 }
-
